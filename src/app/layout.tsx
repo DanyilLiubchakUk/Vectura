95c780b4868012c5266410c8f89588f34f4027a0
DanyilLiubchakUk <danik.liubchak@gmail.com>
2026-01-15 20:56:42 -0500

    feature: add keywords, vieport, and robots to metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,12 +14,52 @@ export const metadata: Metadata = {
     metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL!),
     title: "Vectura",
     description: "Trading strategy backtesting platform",
+    keywords: [
+        "trading",
+        "backtest",
+        "strategy",
+        "algorithmic trading",
+        "finance",
+        "stock market",
+        "quantitative trading",
+        "quant",
+        "backtesting platform",
+        "strategy tester",
+        "automated trading",
+        "trading bot",
+        "historical data",
+        "financial data",
+        "equity curve",
+        "risk management",
+        "performance metrics",
+        "trading software",
+        "backtest strategies",
+        "trading simulator",
+        "investment strategies",
+        "Alpaca",
+        "easy backtesting",
+    ],
+    robots: {
+        index: true,
+        follow: true,
+        googleBot: {
+            index: true,
+            follow: true,
+            "max-image-preview": "large",
+            "max-snippet": -1,
+        },
+    },
     alternates: {
         canonical: '/',
     },
     verification: {
         google: "dMPN-Z0Fn7EY5GBdO1gUZ-zhPG-ivAWogBx5n-zIIuU",
     },
+};
+
+export const viewport = {
+    width: 'device-width',
+    initialScale: 1,
 };
 
 export default function RootLayout({
